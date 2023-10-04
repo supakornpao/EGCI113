ac6c184ce17788d5b3a9383d9b9a3142eaf32cba
supakornpao <146055511+supakornpao@users.noreply.github.com>
2023-10-04 11:10:50 +0700

    Update quadratic.c

--- quadratic.c
+++ quadratic.c
@@ -34,17 +34,17 @@ int main()
     x1=(-b+sqrt(pow(b,2)-4*a*c))/(2*a);
     x2=(-b-sqrt(pow(b,2)-4*a*c))/(2*a);
 
-    if (pow(b,2)<4*a*c)
+    if (pow(b,2)<4*a*c) //When the value in the square root is negative
         printf("\nNo real solution");
 
     else if (a==0){
-            if (b!=0){
+            if (b!=0){ //if a=0 but b!=0, calculate it as a linear equation where x has 1 answer
                 x1=-(float)c/(float)b;
                 printf("\nx is equal to %.1f",x1);
             }
-            else printf("No solution");
+            else printf("No solution"); //if a=0 and b=0, no solution
     }
-    else if (sqrt(pow(b,2)-4*a*c==0))
+    else if (sqrt(pow(b,2)-4*a*c==0)) //The function has only 1 solution if b^2-4ac=0
         printf("\nOne real root is %.1f",x1);
     else
         printf("\nx1 = %.1f, x2 = %.1f",x1,x2);
