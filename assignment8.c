1447df3936c0d25f6e26b6689314a59fbf3b0924
supakornpao <146055511+supakornpao@users.noreply.github.com>
2023-11-04 14:30:13 +0700

    Add files via upload

---
+++ assignment8.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define size 10
+
+int main()
+{
+    int i;
+    int x[size]={};
+
+    for(i=0;i<size;i++){
+        printf("Input score in [%d]: ",i+1);
+        scanf("%d",&x[i]);
+    }
+
+    for(i=0;i<size;i++){
+        printf("%-3d",x[i]);
+    }
+
+    int big=x[0];
+    int small=x[0];
+    for(i=0;i<size;i++){
+        if (big<x[i])
+            big=x[i];
+        if (small>x[i])
+            small=x[i];
+    }
+
+    printf("\nThe highest score is %d", big);
+    printf("\nThe lowest score is %d", small);
+
+    int sum=0;
+    float avg;
+    for(i=0;i<size;i++){
+        sum+=x[i];
+    }
+    avg=(float)sum/(float)size;
+    printf("\nAverage score is: %.2f", avg);
+
+    for(i=0;i<size;i++){
+        if(x[i]>avg){
+            if(x[i]-avg>avg*0.2)
+                printf("\nStudent [%d] got A", i+1);
+            else printf("\nStudent [%d] got C",i+1);
+        }
+        else{
+            if(avg-x[i]>avg*0.2)
+                printf("\nStudent [%d] got F",i+1);
+            else printf("\nStudent [%d] got C",i+1);
+        }
+    }
+
+    return 0;
+}
